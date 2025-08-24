9f7bbbd60e0264d953d3fd4c3f8b151ed0c29014
mathyc0de <mateushackrs7@gmail.com>
2025-08-24 04:20:59 -0300

    ♻️ refactor: change folder bundle logic to one file bundle

--- run.ps1
+++ run.ps1
@@ -1,7 +1,6 @@
 pip install git+https://github.com/mathyc0de/coursera-helper-py3.13
 Write-Host "Starting the download of Coursera Downloader Gui..."
 # Variáveis
-$userprofile = "$env:USERPROFILE\Coursera Downloader Gui"
 $desktop = [Environment]::GetFolderPath("Desktop")
 
 # Baixar a pasta usando GitHub API (zip da pasta)
@@ -9,26 +8,15 @@ $zipUrl = "https://github.com/mathyc0de/coursera-downloader-gui/archive/refs/hea
 $zipFile = "$env:TEMP\coursera-downloader-gui.zip"
 Invoke-WebRequest -Uri $zipUrl -OutFile $zipFile
 
-Write-Host "Extracting to Program Files..."
+Write-Host "Extracting to desktop..."
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 $extractPath = "$env:TEMP\coursera-downloader-gui"
 [System.IO.Compression.ZipFile]::ExtractToDirectory($zipFile, $extractPath)
 
 # Caminho da pasta extraída
-$sourceFolder = Join-Path $extractPath "coursera-downloader-gui-main\build\windows\"
+$sourceFolder = Join-Path $extractPath "coursera-downloader-gui-main\build\"
 
-Write-Host "Creating link do destkop..."
-if (!(Test-Path $userprofile)) {
-    New-Item -ItemType Directory -Path $userprofile | Out-Null
-}
-Copy-Item -Path $sourceFolder\* -Destination $userprofile -Recurse -Force
-
-$WshShell = New-Object -ComObject WScript.Shell
-$shortcut = $WshShell.CreateShortcut("$desktop\Coursera Downloader.lnk")
-$shortcut.TargetPath = Join-Path $userprofile "main.exe"
-$shortcut.WorkingDirectory = $userprofile
-$shortcut.Save()
-
+Copy-Item -Path $sourceFolder\* -Destination $desktop -Recurse -Force
 # Limpar arquivos temporários
 Remove-Item $zipFile -Force
 Remove-Item $extractPath -Recurse -Force
