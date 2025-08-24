b1e9a54b30282c2ef355ded7be644d29c33b7e59
mathyc0de <mateushackrs7@gmail.com>
2025-08-23 22:38:22 -0300

    ♻️ refactor: rename pip package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='coursera-downloader-gui-app',  # Um nome único para o seu pacote
+    name='coursera-downloader-gui',  # Um nome único para o seu pacote
     version='0.1.0',
     packages=find_packages(),
     author='Matheus Silveira',
